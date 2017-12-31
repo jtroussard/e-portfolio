b9167c0c352f6be39a7cedf05ab50830281026f6
Jacques Troussard <jacques.troussard@gmail.com>
2017-12-30 22:26:33 -0500

    add generic configuration file
    -mostly mail server stuff

---
+++ app/lib/configuration.py
@@ -0,0 +1,11 @@
+# Configuration Data
+DEBUG = False
+SECRET_KEY = "super tricky combination"
+
+# Mail Configuration Data
+MAIL_SERVER='smtp.ultraawesomemail.com'
+MAIL_PORT=465
+MAIL_USE_SSL=True
+MAIL_USERNAME = 'sending@mail.address'
+MAIL_PASSWORD = 'not123password'
+MAIL_DEFAULT_SENDER = 'sending@mail.addresss'
