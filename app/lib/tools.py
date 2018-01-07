9e6ba2e298107fb02cb36d2ee67b03ae8305d28a
Jacques Troussard <jacques.troussard@gmail.com>
2018-01-07 17:56:37 -0500

    converted print to logging and cleaned up error logging

--- app/lib/tools.py
+++ app/lib/tools.py
@@ -1,27 +1,26 @@
 import urllib.request
 import json
 import logging
+import datetime
 from lib.vars import *
 
 def get_location(request):
 	ip = request.environ['REMOTE_ADDR']
-	logging.warning("THIS IP ISSSSS -> {}".format(ip))
 	try:
 		with urllib.request.urlopen("https://ipapi.co/{}/json/".format(ip)) as url:
 			data = json.loads(url.read().decode())
-			print(data, flush=True)
 			return data
 	except urllib.error.URLError as err_url:
-		logging.error(err_url.reason)
+		logging.error("[{}]: {}".format(datetime.datetime.now(), err_url.reason))
 		return {'postal': DEFAULT_ZIP}
 	except urllib.error.HTTPError as err_http:
-		loggin.error(err_http.reason)
+		logging.error("[{}]: {}".format(datetime.datetime.now(), err_http.reason))
 		return {'postal': DEFAULT_ZIP}
 
 
 def get_zipcode(data):
 	if not 'postal' in data:
-		return 22193
+		return DEFAULT_ZIP
 	return data['postal']
 
 def nearest_base(data):
@@ -33,15 +32,13 @@ def nearest_base(data):
 				ZIP_API_KEY, ZIP_FORMAT, zip, data, ZIP_UNIT)
 			with urllib.request.urlopen(url) as r_json:
 				distance =  json.loads(r_json.read().decode())['distance']
-				logging.info(distance)
 				if distance < lowest:
 					logging.info("new best zip code {}".format(zip))
 					lowest = distance
 					result = zip
-		logging.info("best zip - {}".format(result))
 		return result
 	except urllib.error.URLError as e:
-		logging.warning(e.reason)
+		logging.warning("[{}]:{}".format(datetime.datetime.now(), e.reason))
 		return nearest_base(data)
 
 
